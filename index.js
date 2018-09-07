b13b1ba75ab48a7fe2422ae506bd7d0abfc9cf5b
Jack Ling <j.lingyx@gmail.com>
2018-09-07 18:28:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function add(a,b){
   return a+b
 }
 
+function subtract(a,b){
+  return b-a
+}
+
 function multiply(a,b){
   return a*b
 }
@@ -15,7 +19,7 @@ function inc(n){
 }
 
 function dec(n){
-  return n--
+  return n-+
 }
 
 function makeInt(n){
